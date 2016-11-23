ae6ebdb5f3913353a4b728526ca78bea012304af
Maxime Schwab <schwabmaxim@gmail.com>
2016-11-23 22:28:54 +0100

    true investment simulation

--- test_model.py
+++ test_model.py
@@ -5,13 +5,17 @@ import sys
 import random
 import numpy as np
 
+
+INITIAL_CAPITAL = 10000.0
+PERCENT_OF_CAPITAL_PER_TRANSACTION = 10.0
+TRANSACTION_FEE = 1.0
+
 def main():
 	model = load_model(sys.argv[1])
 	X = np.load("data/X_test.npy")
 	Y = np.load("data/y_test.npy")
 
-	money = 0
-	total_money = 0
+	money = INITIAL_CAPITAL
 	for i in range(len(X)):
 
 		current = X[i]
@@ -20,15 +24,16 @@ def main():
 		prediction = model.predict(X[i:i+1])
 		if prediction[0][0] > current_value * 1.10:
 			print "---"
-			print "make transaction"
 			print "current money:", money
-			print "total money invested:", total_money
 			print "current stock value:", current_value, "- prediction:", prediction[0][0], "- true value:", Y[i:i+1][0][0]
-			money -= 100.0
-			total_money += 100.0
-			money += prediction[0][0] / Y[i:i+1][0][0] * 100.0
+			investment = money * PERCENT_OF_CAPITAL_PER_TRANSACTION / 100.0
+			money -=  investment + TRANSACTION_FEE * 2.0
+			money += prediction[0][0] / Y[i:i+1][0][0] * investment
+			print "invest:", investment, "- with fee:", TRANSACTION_FEE * 2.0, "- sell:", prediction[0][0] / Y[i:i+1][0][0] * investment, "- gain:", prediction[0][0] / Y[i:i+1][0][0] * investment - investment
+	
 	print ""
-	print "final money:", money, "for", total_money, "invested"
+	print "inital capital:", INITIAL_CAPITAL
+	print "final money:", money
 
 
 if __name__ == "__main__":
