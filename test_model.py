2c0a2044b8925c618eceb15e770ba73b45f954f8
Maxime Schwab <schwabmaxim@gmail.com>
2016-11-23 22:08:53 +0100

    fix test model

--- test_model.py
+++ test_model.py
@@ -18,15 +18,15 @@ def main():
 		current_value = current[0][-1]
 
 		prediction = model.predict(X[i:i+1])
-		if prediction[0][0] > current_value * 1.05:
+		if prediction[0][0] > current_value * 1.10:
 			print "---"
 			print "make transaction"
 			print "current money:", money
 			print "total money invested:", total_money
 			print "current stock value:", current_value, "- prediction:", prediction[0][0], "- true value:", Y[i:i+1][0][0]
-			money -= 100.0 + 2.0
-			total_money += 100.0 + 2.0
-			money += Y[i:i+1][0][0] / current_value * 100.0
+			money -= 100.0
+			total_money += 100.0
+			money += prediction[0][0] / Y[i:i+1][0][0] * 100.0
 	print ""
 	print "final money:", money, "for", total_money, "invested"
 
