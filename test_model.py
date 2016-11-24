e8b4e5efc88fc868e69c19e7d49015bdc5952dd2
Maxime Schwab <maxime@stupeflix.com>
2016-11-24 17:37:16 +0100

    change test model

--- test_model.py
+++ test_model.py
@@ -8,7 +8,7 @@ import numpy as np
 
 INITIAL_CAPITAL = 10000.0
 PERCENT_OF_CAPITAL_PER_TRANSACTION = 10.0
-TRANSACTION_FEE = 1.0
+TRANSACTION_FEE = 0
 
 def main():
 	model = load_model(sys.argv[1])
@@ -16,20 +16,25 @@ def main():
 	Y = np.load("data/y_test.npy")
 
 	money = INITIAL_CAPITAL
+
 	for i in range(len(X)):
 
 		current = X[i]
-		current_value = current[0][-1]
+		current_value = current[0][0]
 
 		prediction = model.predict(X[i:i+1])
-		if prediction[0][0] > current_value * 1.10:
+		if prediction[0][0] > current_value * 1.02:
 			print "---"
 			print "current money:", money
 			print "current stock value:", current_value, "- prediction:", prediction[0][0], "- true value:", Y[i:i+1][0][0]
-			investment = money * PERCENT_OF_CAPITAL_PER_TRANSACTION / 100.0
+			# investment = money * PERCENT_OF_CAPITAL_PER_TRANSACTION / 100.0
+			investment = 100.0
 			money -=  investment + TRANSACTION_FEE * 2.0
-			money += prediction[0][0] / Y[i:i+1][0][0] * investment
-			print "invest:", investment, "- with fee:", TRANSACTION_FEE * 2.0, "- sell:", prediction[0][0] / Y[i:i+1][0][0] * investment, "- gain:", prediction[0][0] / Y[i:i+1][0][0] * investment - investment
+			revenue = Y[i:i+1][0][0] / current_value * investment
+			gain = revenue - investment
+			money += revenue
+
+			print "invest:", investment, "- with fee:", TRANSACTION_FEE * 2.0, "- sell:", revenue, "- gain:", gain
 	
 	print ""
 	print "inital capital:", INITIAL_CAPITAL
